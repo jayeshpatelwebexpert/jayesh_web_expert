67e3ad6bfbe288729cb5b2317f17f04dcb1861ff
Your Name <you@example.com>
2026-08-07 10:45:58 +0530

    Initial website upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,28 +12,6 @@ $(document).ready(function () {
     });
 });
 
-function Marquee(selector, speed) {
-  const parentSelector = document.querySelector(selector);
-  const clone = parentSelector.innerHTML;
-  const firstElement = parentSelector.children[0];
-  let i = 0;
-  console.log(firstElement);
-  parentSelector.insertAdjacentHTML('beforeend', clone);
-  parentSelector.insertAdjacentHTML('beforeend', clone);
-
-  setInterval(function () {
-    firstElement.style.marginLeft = `-${i}px`;
-    if (i > firstElement.clientWidth) {
-      i = 0;
-    }
-    i = i + speed;
-  }, 0);
-}
-
-//after window is completed load
-//1 class selector for marquee
-//2 marquee speed 0.2
-window.addEventListener('load', Marquee('.marquee', 0.2))
 
 let posX = 0,
   posY = 0;
@@ -102,3 +80,27 @@ gsap.utils.toArray(".counter").forEach((counter) => {
     });
 
 });
+
+
+function Marquee(selector, speed) {
+  const parentSelector = document.querySelector(selector);
+  const clone = parentSelector.innerHTML;
+  const firstElement = parentSelector.children[0];
+  let i = 0;
+  console.log(firstElement);
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+
+  setInterval(function () {
+    firstElement.style.marginLeft = `-${i}px`;
+    if (i > firstElement.clientWidth) {
+      i = 0;
+    }
+    i = i + speed;
+  }, 0);
+}
+
+//after window is completed load
+//1 class selector for marquee
+//2 marquee speed 0.2
+window.addEventListener('load', Marquee('.marquee', 0.2))
